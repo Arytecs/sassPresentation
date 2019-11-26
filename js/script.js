fa2cee6bbd6277b24c1d8ce611c5c856eb40786b
Ary <araceliteruel@gmail.com>
2019-11-26 17:54:12 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    //initialising fullpage.js in the jQuery way
+    $(".caja").click(function(element){
+        $(this).addClass('show');
+    })
+    $('#fullpage').fullpage({
+        sectionsColor: ['#ff5f45', '#ffffff', '#fc6c7c', '#62e4ce'],
+        navigation: true,
+        slidesNavigation: true,
+    });
+
+    
+ 
+});
