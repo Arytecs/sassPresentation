37ffb8e13d7580efc8880d9b0fec4a30ff3d2450
Ary <araceliteruel@gmail.com>
2019-11-27 13:52:02 +0100

    Ejemplos e instalación

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         $(this).addClass('show');
     })
     $('#fullpage').fullpage({
-        sectionsColor: ['#ff5f45', '#ffffff', '#fc6c7c', '#62e4ce'],
+        sectionsColor: ['#ff5f45', '#ffffff', '#fc6c7c', '#62e4ce', '#ffffff', '#ffa86d', '#ffffff', '#85f368'],
         navigation: true,
         slidesNavigation: true,
     });
